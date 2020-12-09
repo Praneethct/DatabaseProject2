fd5e103c785ffa8c64f82a57ee406e300fffe3ed
prane <praneethchandra.chandra98@gmail.com>
2020-12-08 23:36:31 -0800

    added toggle button

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from dbconnection import executeQuery
-# from QueryParser import QueryParser
+from QueryParser import QueryParser
 from Parser import getQuery
 
 
@@ -15,12 +15,20 @@ def execute():
     print((request.form["dbType"], request.form["query"]))
     query = request.form["query"].strip().strip('\n').strip('\r').strip('\r\n')
     # parser = QueryParser.getInstance()
-    query = getQuery(query)
-    print("Got query: ", query)
     try:
+        if 'on' in request.form.getlist("free_speech_switch"):
+            print("running free speech")
+            query = getQuery(query)
+        else:
+            print("running parser")
+            parser = QueryParser.getInstance()
+            query = parser.handle(query)
+        
+        print("Got query: ", query)
         tableHeaders, results, timeElapsed = executeQuery(query, request.form["dbType"], request.form["dbName"])
     except:
-        return render_template("index.html")
+        message = "Server faced with unexpected error! Make sure you use the right swtich!!"
+        return render_template("index.html", message=message)
     print(tableHeaders, results)
     return render_template("index.html", tableHeaders=tableHeaders, results=results, timeElapsed=timeElapsed)
 
